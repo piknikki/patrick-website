43c272bd3bdfe27133a674224ad092a4c386cde3
Nikki Petersen <piknik@me.com>
2020-08-14 09:16:10 -0600

    add firebase

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <h1>Our Inventory</h1>
+    <ul v-for="(item, index) in items" :key="index">
+      <li>{{ item.item_id }} | {{ item.name }} | {{ item.price }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
-  name: 'inventory'
+  name: 'inventory',
+  data () {
+    return {
+      items: []
+    }
+  },
+  created () {
+    db.collection('items').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          id: doc.id,
+          item_id: doc.data().item_id,
+          name: doc.data().name,
+          color: doc.data().color,
+          price: doc.data().price
+        }
+        console.log(data)
+        this.items.push(data)
+      })
+    })
+  }
 }
 </script>
 
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyAy5LLe_kGSGjX79ElyaMZhIyDq_Gt8ta8',
+  authDomain: 'complete-forklift.firebaseapp.com',
+  databaseURL: 'https://complete-forklift.firebaseio.com',
+  projectId: 'complete-forklift',
+  storageBucket: 'complete-forklift.appspot.com',
+  messagingSenderId: '996984972376',
+  appId: '1:996984972376:web:6fc2650bca11e3e078562d',
+  measurementId: 'G-7DVKETSTVB'
+}
